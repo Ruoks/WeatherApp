3b40c752cef011b3c4002865c65cde76f643ff1c
Ruoks <rookaaz@gmail.com>
2022-02-19 13:54:04 +0000

    Add of project

---
+++ BradyWeather/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BradyWeather.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
